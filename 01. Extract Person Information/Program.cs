753b23c3d70e3094beecf2bbab40fc3a2fa061a0
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-15 16:28:55 +0200

    Add project files.

---
+++ 01. Extract Person Information/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string text = Console.ReadLine();
+
+    int nameStart = text.IndexOf('@') + 1;
+    int nameEnd = text.IndexOf('|');
+    string name = text.Substring(nameStart, nameEnd - nameStart);
+
+    int ageStart = text.IndexOf('#') + 1;
+    int ageEnd = text.IndexOf('*');
+    string age = text.Substring(ageStart, ageEnd - ageStart);
+
+    Console.WriteLine($"{name} is {age} years old.");
+}
