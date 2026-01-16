7517a4144ace9576a079860a987864154a2b00b8
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-16 22:12:35 +0200

    Added a New Project

---
+++ 02. Ascii Sumator/Program.cs
@@ -0,0 +1,18 @@
+﻿char firstChar = char.Parse(Console.ReadLine());
+char secondChar = char.Parse(Console.ReadLine());
+string text = Console.ReadLine();
+
+int start = Math.Min(firstChar, secondChar);
+int end = Math.Max(firstChar, secondChar);
+
+int sum = 0;
+
+foreach (char ch in text)
+{
+    if (ch > start && ch < end)
+    {
+        sum += ch;
+    }
+}
+
+Console.WriteLine(sum);
