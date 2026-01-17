968e6ea8181d5468ad97b88e23c69db7b588ed77
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-17 10:56:14 +0200

    Added a New Project

---
+++ 03. Treasure Finder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+int[] key = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+string input;
+while ((input = Console.ReadLine()) != "find")
+{
+    StringBuilder decrypted = new StringBuilder();
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        int keyValue = key[i % key.Length];
+        char decryptedChar = (char)(input[i] - keyValue);
+        decrypted.Append(decryptedChar);
+    }
+
+    string result = decrypted.ToString();
+
+    int typeStart = result.IndexOf('&') + 1;
+    int typeEnd = result.IndexOf('&', typeStart);
+    string type = result.Substring(typeStart, typeEnd - typeStart);
+
+    int coordStart = result.IndexOf('<') + 1;
+    int coordEnd = result.IndexOf('>');
+    string coordinates = result.Substring(coordStart, coordEnd - coordStart);
+
+    Console.WriteLine($"Found {type} at {coordinates}");
+}
