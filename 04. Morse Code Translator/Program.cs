ca5ca58764406e8c446b9bd6f966ff7db55db1e8
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-19 16:24:30 +0200

    Added a New Project

---
+++ 04. Morse Code Translator/Program.cs
@@ -0,0 +1,27 @@
+﻿Dictionary<string, char> morse = new Dictionary<string, char>
+        {
+            {".-", 'A'},   {"-...", 'B'}, {"-.-.", 'C'}, {"-..", 'D'},
+            {".", 'E'},    {"..-.", 'F'}, {"--.", 'G'},  {"....", 'H'},
+            {"..", 'I'},   {".---", 'J'}, {"-.-", 'K'},  {".-..", 'L'},
+            {"--", 'M'},   {"-.", 'N'},   {"---", 'O'},  {".--.", 'P'},
+            {"--.-", 'Q'}, {".-.", 'R'},  {"...", 'S'},  {"-", 'T'},
+            {"..-", 'U'},  {"...-", 'V'}, {".--", 'W'},  {"-..-", 'X'},
+            {"-.--", 'Y'}, {"--..", 'Z'}
+        };
+
+string input = Console.ReadLine();
+
+string[] words = input.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+List<string> result = new List<string>();
+
+foreach (string word in words)
+{
+    string translatedWord = string.Concat(
+        word.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(code => morse[code])
+    );
+
+    result.Add(translatedWord);
+}
+
+Console.WriteLine(string.Join(" ", result));
