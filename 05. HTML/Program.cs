61c7a23199ec98713a5e2014f42dfa13eaa4890b
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-20 06:58:51 +0200

    Added a New Project

---
+++ 05. HTML/Program.cs
@@ -0,0 +1,18 @@
+﻿string title = Console.ReadLine();
+string content = Console.ReadLine();
+
+Console.WriteLine($"<h1>");
+Console.WriteLine(title);
+Console.WriteLine($"</h1>");
+
+Console.WriteLine($"<article>");
+Console.WriteLine(content);
+Console.WriteLine($"</article>");
+
+string comment;
+while ((comment = Console.ReadLine()) != "end of comments")
+{
+    Console.WriteLine($"<div>");
+    Console.WriteLine(comment);
+    Console.WriteLine($"</div>");
+}
